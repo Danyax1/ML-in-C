f02d5524f38668d32bc4d980369479d9ff5ab093
Morales Daniel <whod0977@gmail.com>
2025-08-01 23:43:04 +0300

    Save nn in config

--- file_nn_io/main.c
+++ file_nn_io/main.c
@@ -9,51 +9,6 @@
 int main(){
 
     srand(time(0));
-    // srand(12);
-
-    // Matrix m1  = mt_create(6, 5);
-    // Matrix m2  = mt_create(5, 6);
-    // Matrix res = mt_create(6, 6);
-
-    // mt_fill(m1, 1);
-    // mt_fill(m2, -2);
-
-    // // mt_print(m2);
-    // // mt_swap_row(m2, 4, 2);
-    // // mt_print(m2);
-    // // Matrix m = mt_create(3, 3);
-    // // for (int i = 0; i < 3; i++)
-    // //     for (int j = 0; j < 3; j++)
-    // //         mt_pos(m, i, j) = i * 10 + j;
-
-    // // mt_print(m);
-    // // mt_swap_col(m, 0, 2);
-    // // mt_print(m);
-    // mt_mult(res, m1, m2);
-
-    // mt_print(res);
-
-    // Matrix m_det = mt_create(2, 2);
-    // mt_pos(m_det, 0, 0) = 0;
-    // mt_pos(m_det, 0, 1) = 1;
-    // mt_pos(m_det, 1, 0) = 4;
-    // mt_pos(m_det, 1, 1) = 6;
-
-    // mt_print(m_det);
-
-    // printf("Determinant of res: %f\n", mt_det(m_det));
-    // printf("----------------------------\n");
-    // mt_activate(res, sigmoid);
-    // mt_print(res);
-
-    // Matrix id = mt_create(6, 6);;
-    // mt_id(res);
-    // mt_copy(id, res);
-    // mt_print(id);
-
-
-    // mt_free(&id);
-    // mt_free(&res);
 
     float dataset[N_SAMPLES][INPUT_SIZE + OUTPUT_SIZE] = {
       // A1 A0 B1 B0|Sum
@@ -98,7 +53,7 @@ int main(){
     N_Net grad = create_n_net(l_count, arch_len, arch);
     float rate = -(1);
 
-    for(int iter = 0; iter < 50000; iter++){
+    for(int iter = 0; iter < 1000; iter++){
 
         float loss = 0;
 
@@ -122,6 +77,45 @@ int main(){
         printf("%d%d + %d%d = %f\n", (int)dataset[i][0], (int)dataset[i][1], (int)dataset[i][2],(int)dataset[i][3], out*6.0f);
     }
 
+    // save config in config.txt
 
+    /*
+    1: arch_len
+    2: arch
+    3: w_n
+    4: b_n
+    */
+
+    FILE *config = fopen("config.txt", "w");
+    assert(config != NULL);
+    fprintf(config, "%d\n", nn.arch_len);
+
+    for(int i = 0; i < nn.arch_len; i++){
+        fprintf(config, "%d ", nn.arch[i]);
+    }
+    fprintf(config, "\n");
+
+    for(int i = 0; i < nn.l_count; i++){
+        for(int j = 0; j < nn.w_n[i].rows; j++){
+            for(int k = 0; k < nn.w_n[i].cols; k++){
+                fprintf(config, "%f ", mt_pos(nn.w_n[i], j, k));
+            }
+            fprintf(config, "\n");
+        }
+        fprintf(config, "\n");
+    }
+    fprintf(config, "\n");
+    for(int i = 0; i < nn.l_count; i++){
+        for(int j = 0; j < nn.b_n[i].rows; j++){
+            for(int k = 0; k < nn.b_n[i].cols; k++){
+                fprintf(config, "%f ", mt_pos(nn.b_n[i], j, k));
+            }
+            fprintf(config, "\n");
+        }
+        fprintf(config, "\n");
+    }
+
+    fclose(config);
+    print_n_net(nn);
     return 0;
 }
