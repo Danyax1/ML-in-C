fca9c31b6ae975e7704147b6514d5383b88456be
Morales Daniel <whod0977@gmail.com>
2025-08-03 11:01:32 +0300

    Norm the output matrix

--- file_nn_io/main.c
+++ file_nn_io/main.c
@@ -6,6 +6,25 @@
 #define INPUT_SIZE 4
 #define OUTPUT_SIZE 1
 
+void norm_out(Matrix out, float* scale, float* shift){
+        float max = mt_pos(out, 0, 0);
+    float min = mt_pos(out, 0, 0);
+    for (int i = 0; i < out.rows; i++) {
+        if(mt_pos(out, i, 0) > max){
+            max = mt_pos(out, i, 0);
+        }
+        if(mt_pos(out, i, 0) < min){
+            min = mt_pos(out, i, 0);
+        }
+    }
+    *scale = max - min;
+    *shift = max/(*scale) - 1;
+
+    for (int i = 0; i < out.rows; i++) {
+        mt_pos(out, i, 0) = mt_pos(out, i, 0)/(*scale) - *shift;
+    }
+}
+
 int main(){
 
     srand(time(0));
@@ -37,11 +56,9 @@ int main(){
 
     split_dataset(&dataset[0][0], INPUT_SIZE, OUTPUT_SIZE, N_SAMPLES, inputs, outputs);
 
-    for (int i = 0; i < outputs.rows; i++) {
-        mt_pos(outputs, i, 0) /= 6.0f;
-    }
-    //  mt_print(inputs);
-    //  mt_print(outputs);
+    float scale;
+    float shift;
+    norm_out(outputs, &scale, &shift);
 
     int arch[] = {INPUT_SIZE, 2*INPUT_SIZE, INPUT_SIZE,  1};
     int arch_len = sizeof(arch)/sizeof(arch[0]);
@@ -53,7 +70,7 @@ int main(){
     N_Net grad = create_n_net(l_count, arch_len, arch);
     float rate = (1);
 
-    train_n_net(nn, grad, inputs, outputs, N_SAMPLES, rate, 10000, true);
+    train_n_net(nn, grad, inputs, outputs, N_SAMPLES, rate, 1000000, false);
 
     printf("------------------------------\n");
     
@@ -61,8 +78,10 @@ int main(){
         set_n_net_input(nn, mt_row(inputs, i));
         forward_n_net(nn);
 
+        printf("Final loss: %f\n", loss_n_net(nn, mt_row(outputs, i)));
+
         float out = mt_pos(output_n_net(nn), 0, 0);
-        printf("%d%d + %d%d = %f\n", (int)dataset[i][0], (int)dataset[i][1], (int)dataset[i][2],(int)dataset[i][3], out*6.0f);
+        printf("%d%d + %d%d = %f\n", (int)dataset[i][0], (int)dataset[i][1], (int)dataset[i][2],(int)dataset[i][3], (out+shift)*scale);
     }
     printf("------------------------------\n");
     return 0;
